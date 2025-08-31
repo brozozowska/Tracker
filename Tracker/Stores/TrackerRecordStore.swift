bb1a8a3df9baa2907eab8df1271c6a579bf2c23a
brozozowska <s_rozov@me.com>
2025-08-31 18:22:31 +0300

    feat(model): создан слой для работы с данными дополнительно трансформеры перемещены в корень проекта

---
+++ Tracker/Stores/TrackerRecordStore.swift
@@ -0,0 +1,27 @@
+//
+//  TrackerRecordStore.swift
+//  Tracker
+//
+//  Created by Сергей Розов on 31.08.2025.
+//
+
+import UIKit
+import CoreData
+
+final class TrackerRecordStore {
+    
+    // MARK: - Private Properties
+    private let context: NSManagedObjectContext
+    
+    // MARK: - Initializers
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
+    convenience init() {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        self.init(context: context)
+    }
+    
+    // MARK: - Methods
+}
---
+++ Tracker/Stores/TrackerStore.swift
@@ -0,0 +1,27 @@
+//
+//  TrackerStore.swift
+//  Tracker
+//
+//  Created by Сергей Розов on 31.08.2025.
+//
+
+import UIKit
+import CoreData
+
+final class TrackerStore {
+    
+    // MARK: - Private Properties
+    private let context: NSManagedObjectContext
+    
+    // MARK: - Initializers
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
+    convenience init() {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        self.init(context: context)
+    }
+    
+    // MARK: - Methods
+}
